4c706dca1b3da7b813cce99707838fd2cad93aa5
Ezhilan <ezhilan@centative.com>
2020-04-09 21:02:47 +0530

    Fixed UI issue in 'contacts'

--- app/src/main/java/com/sample/engage/ui/contacts/Contacts_adapter.kt
+++ app/src/main/java/com/sample/engage/ui/contacts/Contacts_adapter.kt
@@ -1,6 +1,5 @@
 package com.sample.engage.ui.contacts
 
-import android.content.res.ColorStateList
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
@@ -16,25 +15,29 @@ import kotlin.random.Random
 class Contacts_adapter : ListAdapter<String, Contacts_adapter.ViewHolder>(DiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_contact, parent, false))
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_contact, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(position)
     }
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var name: TextView = view.findViewById(R.id.contact_name)
         var initial: TextView = view.findViewById(R.id.initial)
         var initialBg: CardView = view.findViewById(R.id.initialCardView)
-        fun bind(position: Int){
+        fun bind(position: Int) {
             val contactName = getItem(position)
             name.text = contactName
             initial.text = "${contactName.first()}"
             val rnd = Random
-            val bubbleColor: Int = Color.argb(125, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+            val bubbleColor = Color.argb(.25F, Random.nextFloat(), Random.nextFloat(), Random.nextFloat())
             initialBg.setCardBackgroundColor(bubbleColor)
-            initial.setTextColor(ContrastColor(Color.valueOf(bubbleColor)))
+            initial.setTextColor(getContrastColor(bubbleColor))
+            if(position%2==0)
+                name.setBackgroundColor(Color.parseColor("#F9F9F9"))
         }
     }
 
@@ -50,15 +53,14 @@ class Contacts_adapter : ListAdapter<String, Contacts_adapter.ViewHolder>(DiffCa
     }
 }
 
-fun ContrastColor(color: Color): Int {
-    var d = 0
+fun getContrastColor(color: Int): Int {
+    var d = 127
 
     // Counting the perceptive luminance - human eye favors green color...
-    val luminance: Double = (0.299 * color.red() + 0.587 * color.green()  + 0.114 * color.blue() ) / 255
+    val luminance =
+        1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color)) / 255
+    if (luminance < 0.5)
+        d = 255
 
-    d = if (luminance > 0.5) 0 // bright colors - black font
-    else 255 // dark colors - white font
-
-
-    return Color.argb(255, d,d,d)
+    return Color.argb(255, d, d, d)
 }
